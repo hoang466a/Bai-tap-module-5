5afac887382a1e94eb4d2de8277f33715ca10119
hoang466a <hoangtn96@gmail.com>
2022-05-11 06:09:54 +0700

    day bai tap ngay 10-05-2022

--- case_study/webstorm/angular-case-study/src/app/app.module.ts
+++ case_study/webstorm/angular-case-study/src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ListServiceComponent } from './list-service/list-service.component';
-import { EditServiceComponent } from './edit-service-room/edit-service.component';
-import { CreateServiceComponent } from './create-service-house/create-service.component';
-import { ListCustomerComponent } from './list-customer/list-customer.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
-import { CreateCustomerComponent } from './create-customer/create-customer.component';
-import { CreateContractComponent } from './create-contract/create-contract.component';
-import { ListContractComponent } from './list-contract/list-contract.component';
+import { ListServiceComponent } from './service-furama/list-service/list-service.component';
+import { EditServiceComponent } from './service-furama/edit-service-room/edit-service.component';
+import { CreateServiceComponent } from './service-furama/create-service-house/create-service.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { CreateContractComponent } from './contract/create-contract/create-contract.component';
+import { ListContractComponent } from './contract/list-contract/list-contract.component';
 import { RoomComponent } from './room/room.component';
-import { CreateServiceRoomComponent } from './create-service-room/create-service-room.component';
-import { CreateServiceVillaComponent } from './create-service-villa/create-service-villa.component';
-import { EditServiceHouseComponent } from './edit-service-house/edit-service-house.component';
-import { EditServiceVillaComponent } from './edit-service-villa/edit-service-villa.component';
+import { CreateServiceRoomComponent } from './service-furama/create-service-room/create-service-room.component';
+import { CreateServiceVillaComponent } from './service-furama/create-service-villa/create-service-villa.component';
+import { EditServiceHouseComponent } from './service-furama/edit-service-house/edit-service-house.component';
+import { EditServiceVillaComponent } from './service-furama/edit-service-villa/edit-service-villa.component';
 
 @NgModule({
   declarations: [
