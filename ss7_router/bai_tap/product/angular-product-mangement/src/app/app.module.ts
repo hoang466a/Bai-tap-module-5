907de628c2abdc89c5e926d0a6464b46c4083031
hoang466a <hoangtn96@gmail.com>
2022-05-16 20:27:17 +0700

    day bai tap ngay 15-05-2022

---
+++ ss7_router/bai_tap/product/angular-product-mangement/src/app/app.module.ts
@@ -0,0 +1,31 @@
+  import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import {FormGroup, FormsModule,ReactiveFormsModule} from "@angular/forms";
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductCreateComponent,
+    ProductListComponent,
+    ProductEditComponent,
+    ProductDeleteComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
